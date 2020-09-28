9567b0dbc2a51285f1595faa43dce136844b0838
zifanf2 <zifanfeng@gmail.com>
2020-09-27 22:47:36 -0500

    fix test

--- src/test/java/com/alibaba/json/bvt/guava/HashMultimapTest.java
+++ src/test/java/com/alibaba/json/bvt/guava/HashMultimapTest.java
@@ -1,10 +1,12 @@
 package com.alibaba.json.bvt.guava;
+import static net.javacrumbs.jsonunit.assertj.JsonAssertions.assertThatJson;
 
 import com.alibaba.fastjson.JSON;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.common.collect.HashMultimap;
 import com.google.gson.Gson;
 import junit.framework.TestCase;
+import net.javacrumbs.jsonunit.core.Option;
 
 /**
  * Created by wenshao on 15/01/2017.
@@ -16,6 +18,12 @@ public class HashMultimapTest extends TestCase {
         map.put("name", "b");
 
         String json = JSON.toJSONString(map);
-        assertEquals("{\"name\":[\"a\",\"b\"]}", json);
+        assertThatJson("{\"name\":[\"b\",\"a\"]}")
+        .when(Option.IGNORING_ARRAY_ORDER)
+        .isEqualTo(json);
+        
+        assertThatJson("{\"name\":[\"a\",\"b\"]}")
+        .when(Option.IGNORING_ARRAY_ORDER)
+        .isEqualTo(json);
     }
 }
